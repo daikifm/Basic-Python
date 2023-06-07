0b58341efae9810097234b388c8f0df79518d3d8
daikifm <daikifm8255@gmail.com>
2023-06-07 12:11:09 +0900

    関数完了

--- prime_number.py
+++ prime_number.py
@@ -1,15 +1,27 @@
-a = int(input("aの値を入力: "))
+a =float(input("aの値を入力: "))
 
-limit = a ** (1/2)
+def prime(n):
+    if n < 0:
+        return print("負の値です")
+    elif n.is_integer() == False:
+        return print("少数です")
 
-count = 2
-count2 = 0
+    limit = n ** (1/2)
 
-while count <= limit:
-    if a % count == 0:
-        print(str(a) + "は素数でない")
-        count2 = 1
-        break
-    count += 1
-if count2 != 1:
-    print(str(a) + "は素数である")
+    count = 2
+    count2 = 0
+
+    while count <= limit:
+        if n % count == 0:
+            count2 = 1
+            break
+        count += 1
+
+    if count2 != 1:
+        return print(str(n) + "は素数である")
+    else: 
+        return print(str(n) + "は素数でない")
+
+prime(a)
+
+
